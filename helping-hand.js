d10253e219e767ee12ae6479576552bd2b190f6d
hugo <hugo.oconnor@gmail.com>
2015-06-07 14:54:53 +1000

    added glyphicons

--- helping-hand.js
+++ helping-hand.js
@@ -2,6 +2,15 @@ Reports = new Mongo.Collection("reports");
 
 
 if (Meteor.isClient) {
+
+  //function to add report
+  AddReport = function () {
+    
+  }
+
+  //function to calculate user health
+
+  //
  
 }
 
