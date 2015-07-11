35c92f25c853c2149d3391f6363989fa9b55e6e2
hugo <hugo.oconnor@gmail.com>
2015-07-12 09:02:48 +1000

    added email, modified how reports appear

--- helping-hand.js
+++ helping-hand.js
@@ -1,6 +1,9 @@
 //Create Reports collection on 'meteor reset'
 Reports = new Mongo.Collection("reports");
 
+//Create Settings collection on 'meteor reset'
+Settings = new Mongo.Collection("settings");
+
 //Confidence slider
 xyz = function(event, ui){
           var confidence = $("#slider").slider("value");
@@ -26,7 +29,8 @@ if (Meteor.isClient) {
 
     //return report data
     reports: function () {
-      return Reports.find({});
+      var start = new Date(Date.now()-14*24*60*60*1000);
+      return Reports.find({ created: { $gt: start} } );
     },
 
     //return true if report(s)
@@ -87,4 +91,33 @@ Template.report.helpers({
 
 //All server javascript
 if (Meteor.isServer) {
+
+Meteor.startup(function () {
+  process.env.MAIL_URL = 'smtp://postmaster%40mg.helpinghand.io:password@smtp.mailgun.org:587';
+  });
+
+  Meteor.methods({
+  sendEmail: function (to, subject, text) {
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    // don’t allow sending email unless the user is logged in
+    //if (!Meteor.user())
+      //throw new Meteor.Error(403, 'not logged in');
+
+    // and here is where you can throttle the number of emails this user
+    // is allowed to send per day
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
+});
+
+
+
 };
