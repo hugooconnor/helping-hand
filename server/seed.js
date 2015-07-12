6e2c7dd16fdbec5b76f4a8319466a92e036afc9a
hugo <hugo.oconnor@gmail.com>
2015-07-12 21:10:50 +1000

    made it more differenter

---
+++ server/seed.js
@@ -0,0 +1,11 @@
+//Create Reports collection on 'meteor reset'
+Reports = new Mongo.Collection("reports");
+
+//Create Settings collection on 'meteor reset'
+Settings = new Mongo.Collection("settings");
+
+//Create Settings collection on 'meteor reset'
+Invites = new Mongo.Collection("invites");
+
+//Create Settings collection on 'meteor reset'
+Emails = new Mongo.Collection("emails");
