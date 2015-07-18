328e33cf36fb48ea9cedc88bd267904483d811fc
hugo <hugo.oconnor@gmail.com>
2015-07-18 13:43:34 +1000

    added email server functions

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,4 @@
 Meteor.startup(function () {
+  //environment variable for email sending
+  process.env.MAIL_URL = 'smtp://postmaster%40mg.helpinghand.io:password@smtp.mailgun.org:587';
 });
--- app/server/methods.js
+++ app/server/methods.js
@@ -2,10 +2,22 @@
 /* Server Only Methods */
 /*****************************************************************************/
 Meteor.methods({
-  /*
-   * Example:
-   *
-   * '/app/items/insert': function (item) {
-   * }
-   */
+  sendEmail: function (to, from, subject, text) {
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+    // don’t allow sending email unless the user is logged in
+    //if (!Meteor.user())
+      //throw new Meteor.Error(403, 'not logged in');
+    // and here is where you can throttle the number of emails this user
+    // is allowed to send per day
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  },
+
+
 });
