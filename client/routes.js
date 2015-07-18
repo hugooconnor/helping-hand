0ef063ebd3f2416974aa1bbb4f2793a9ab60cdb6
hugo <hugo.oconnor@gmail.com>
2015-07-18 13:28:30 +1000

    got rid of old code

--- client/routes.js
+++
@@ -1,5 +0,0 @@
-Router.route('/', function () {
-      // render the Home template with a custom data context
-         this.render('index');
-         });
-     
--- client/templates/error.js
+++
@@ -1,7 +0,0 @@
-Meteor.call("methodName", function (error) {
-  // identify the error
-  if (error.error === "create-failed") {
-    // show a nice error message
-    Session.set("errorMessage", "Please log in to post a comment.");
-  }
-});
