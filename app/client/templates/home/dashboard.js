e8cf0120a93ee08eb79e7ce86fd379c2f37b3dd2
hugo <hugo.oconnor@gmail.com>
2015-07-18 22:09:11 +1000

    added anon field to reports

--- app/client/templates/home/dashboard.js
+++ app/client/templates/home/dashboard.js
@@ -11,8 +11,17 @@ Template.dashboard.helpers({
   reports: function () {
       var start = new Date(Date.now()-14*24*60*60*1000);
       return Reports.find({helpee: Meteor.user().username, created: { $gt: start}});
-    }
+    },
 
   //add health calculation here.
+
+  hearts: function(n) {
+    if(n < 100 && n >= 80)
+      return "<i class='icon ion-heart></i>";
+  },
+
+  who: function() {
+    return Meteor.user().username;
+  }
 
 });
